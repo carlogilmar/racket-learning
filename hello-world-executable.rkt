176db290b843eeb6cf9d8c0ccee03229c4726bf8
carlogilmar <carlogilmar12@gmail.com>
2018-11-19 16:05:54 -0600

    Adding first examples using racket

---
+++ hello-world-executable.rkt
@@ -0,0 +1,12 @@
+#! /usr/bin/env racket
+
+(define (extract str)
+    (substring str 4 7))
+
+(extract "the cat out of the bag")
+
+|#
+  Esto es un comentario multilinea
+
+  > racket hello-world.rkt
+#|
---
+++ hello-world.rkt
@@ -0,0 +1,12 @@
+#lang racket
+
+(define (extract str)
+    (substring str 4 7))
+
+(extract "the cat out of the bag")
+
+|#
+  Esto es un comentario multilinea
+
+  > racket hello-world.rkt
+#|
